4b94606456ede367aec5cc7eaeed005ff912c0bd
Ayush Kumar Karmi <ayushkumarkarmi30@gmail.com>
2026-06-30 08:36:26 +0530

    Updated the folder Structure

---
+++ src/config/serverDetails.js
@@ -0,0 +1,32 @@
+const SERVER_1_PORT = process.env.SERVER_1_PORT || 3001;
+const SERVER_2_PORT = process.env.SERVER_2_PORT || 3002;
+const SERVER_3_PORT = process.env.SERVER_3_PORT || 3003;
+
+const SERVER_DETAILS = [
+    {
+        "id": 1,
+        "name": "SERVER 1",
+        "port": SERVER_1_PORT,
+        "host": "localhost",
+        "url": `http://localhost:${SERVER_1_PORT}`,
+        "alive": true
+    },
+    {
+        "id": 2,
+        "name": "SERVER 2",
+        "port": SERVER_2_PORT,
+        "host": "localhost",
+        "url": `http://localhost:${SERVER_2_PORT}`,
+        "alive": true
+    },
+    {
+        "id": 3,
+        "name": "SERVER 3",
+        "port": SERVER_3_PORT,
+        "host": "localhost",
+        "url": `http://localhost:${SERVER_3_PORT}`,
+        "alive": true
+    }
+]
+
+export default SERVER_DETAILS
